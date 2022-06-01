8fd223d0c0bc531a84363c30b4e1552d6b6d9643
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-01 11:07:56 +0200

    completed first live coding

--- js/object.js
+++ js/object.js
@@ -30,3 +30,20 @@ palla.somma();
 
 // se voglio aggiungere elementi dentro materiali, che è un array si fa così:
 palla.materiali.push("carta");
+
+// come ciclare su un oggetto
+//useremo il for..in in questo modo:
+//for(let key in palla){} // key e palla sono i valori che posso dare io e chiamarli in qualsiasi modo
+
+for(let key in palla){
+    console.log(key);
+    // quale è il valore che corrisponde alla chiave colore?
+    console.log(palla[key])
+
+    if(key === "materiali"){
+        for(let i = 0; i< palla.materiali.length; i++){
+            console.log(" - ", palla.materiali[i]);
+        }
+    }
+}
+
